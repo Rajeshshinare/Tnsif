ee26593d662c363f2b79e50f151c030b3378d01f
RAJESH <RAJESH@Rajesh-Shravni17>
2025-07-10 12:16:12 +0530

    Day 12

---
+++ Day 12/src/com/tnsif/day12/MultiThreading/ChildDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.day12.MultiThreading;
+
+public class ChildDemo {
+
+	public static void main(String[] args) {
+		
+		ChildThread c = new ChildThread (10, "Hello");
+		ChildThread c1 = new ChildThread (10, "Rajesh");
+		
+		c.start();
+		c1.start();
+		System.out.println("End of the Program");
+	}
+}
