1cf4ca1480996618006d2821c818c7461d4f839b
RAJESH <RAJESH@Rajesh-Shravni17>
2025-07-11 11:38:19 +0530

    Day 13

---
+++ Day 13/src/com/tnsif/day13/synchronization/AccountThread.java
@@ -0,0 +1,29 @@
+package com.tnsif.day13.synchronization;
+
+public class AccountThread extends Thread {
+
+	private Account acc;
+	private int amt;
+	
+	//	parameter constructor 
+	public AccountThread(Account acc, int amt) {
+		super();
+		this.acc = acc;
+		this.amt = amt;
+		start();
+	}
+	
+	@Override
+	public void run()
+	{
+		try
+		{
+			acc.withdraw(amt);
+		}
+		
+		catch(InsufficientBalanceException e)
+		{
+			System.err.println(e.getMessage());
+		}
+	}	
+}
